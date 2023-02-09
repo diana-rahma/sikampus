c37396c58711aa31c2341c6ced05f9f34e3869a2
diana-rahma <98369663+diana-rahma@users.noreply.github.com>
2023-02-09 14:45:01 +0700

    adminlte

---
+++ conf/config.php
@@ -0,0 +1,12 @@
+<?php
+$koneksi = mysqli_connect('localhost', 'root', '', 'db_sikampus');
+
+// if(!$koneksi){
+//     die("koneksi gagal". mysqli_connect_error());
+// }
+// else{
+//     echo "koneksi berhasil";
+// }
+
+
+?>
